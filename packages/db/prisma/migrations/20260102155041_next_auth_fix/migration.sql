b9ef09ff48ed9d72011dcfdf9af1891a2ab9dce7
eru-U <180927586+eru-U@users.noreply.github.com>
2026-01-03 00:51:01 +0900

    refactor: nextauth似合う形に修正

---
+++ packages/db/prisma/migrations/20260102155041_next_auth_fix/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - The primary key for the `VerificationToken` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `identified` on the `VerificationToken` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[sessionToken]` on the table `Session` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[token]` on the table `VerificationToken` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[identifier,token]` on the table `VerificationToken` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `identifier` to the `VerificationToken` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Account" ADD COLUMN     "type" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "VerificationToken" DROP CONSTRAINT "VerificationToken_pkey",
+DROP COLUMN "identified",
+ADD COLUMN     "identifier" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
