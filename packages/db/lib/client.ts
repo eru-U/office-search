80554b10e3413dee639cdaa06c00d85191987622
eru <180927586+eru-U@users.noreply.github.com>
2026-01-08 04:42:48 +0900

    add: 色々導入

--- packages/db/lib/client.ts
+++ packages/db/lib/client.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 /// 開発環境でのホットリロードによる接続過多を防ぐおまじない
