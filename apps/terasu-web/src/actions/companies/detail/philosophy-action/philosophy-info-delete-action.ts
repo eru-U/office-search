b3e4d882a1d11cda82c9004d86b1e34b4cae3559
eru <180927586+eru-U@users.noreply.github.com>
2026-01-16 07:24:10 +0900

    add: 企業理念削除用のサーバアクションの追加

---
+++ apps/terasu-web/src/actions/companies/detail/philosophy-action/philosophy-info-delete-action.ts
@@ -0,0 +1,13 @@
+import { prismaClient } from "@terasu/db";
+
+/**
+ * 企業理念IDを受け取ってデータを削除する関数
+ * @param id 企業理念ID
+ */
+export const philosophyInfoDeleteAction = async (id: string) => {
+  await prismaClient.companyPhilosophy.delete({
+    where: {
+      id: id,
+    },
+  });
+};
