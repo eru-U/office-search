490abcbe7d2134d7431155594858a9757dad79af
eru <180927586+eru-U@users.noreply.github.com>
2026-01-16 06:04:25 +0900

    refactor: contextで渡すようになり不要になったため型を削除

--- apps/terasu-web/src/app/(authenticated)/companies/[Id]/components/details/types.ts
+++ apps/terasu-web/src/app/(authenticated)/companies/[Id]/components/details/types.ts
@@ -42,8 +42,3 @@ export interface YearDateData {
 }
 
 export type CacheData = Record<string, YearlyDetailItem[]>;
-
-export interface DetailDataProps {
-  data: YearlyDetailItem;
-  onRefresh: () => void;
-}
