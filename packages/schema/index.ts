b328a6c65a9b61d7c3554e011261455787b79c24
eru <180927586+eru-U@users.noreply.github.com>
2026-01-16 06:32:11 +0900

    refactor: スキーマの移動and追加

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -1,2 +1,4 @@
+export * as companyDetailHeaderSchema from "./models/companyDetailHeaderSchema";
+export * as companyDetailPhilosophySchema from "./models/companyDetailPhilosophySchema";
 export * as companyDetailSchema from "./models/companyDetailSchema";
 export * as companySchema from "./models/companySchema";
