9e2d92372ecc5ae12093eccbb4d1e8d3336e07b6
eru-U <180927586+eru-U@users.noreply.github.com>
2026-01-16 10:12:14 +0900

    add: 先ほど定義したschemaをexport

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -1,4 +1,5 @@
 export * as companyDetailHeaderSchema from "./models/companyDetailHeaderSchema";
 export * as companyDetailPhilosophySchema from "./models/companyDetailPhilosophySchema";
 export * as companyDetailSchema from "./models/companyDetailSchema";
+export * as companyDetailWelfareSchema from "./models/companyDetailWelfareSchema";
 export * as companySchema from "./models/companySchema";
