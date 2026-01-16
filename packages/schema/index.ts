a6e10424b3d6db693ad15e388a0ae5417da37790
eru-U <180927586+eru-U@users.noreply.github.com>
2026-01-16 12:30:36 +0900

    add: 新しく作成したスキーマの定義

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -1,3 +1,4 @@
+export * as companyDetailBranchSchema from "./models/companyDetailBranchSchema";
 export * as companyDetailHeaderSchema from "./models/companyDetailHeaderSchema";
 export * as companyDetailPhilosophySchema from "./models/companyDetailPhilosophySchema";
 export * as companyDetailSchema from "./models/companyDetailSchema";
